367001c508da6f0fc6826d4d5c8e8428c7bf2284
Tu Hoang <rebyn@me.com>
2013-06-08 14:33:14 +0200

    add _map partial

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def index
+    @markers = Hydrant.where("latitude IS NOT ?", nil)
+    @center = Hydrant.where("latitude IS NOT ?", nil).first
   end
 end
