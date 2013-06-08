0ca8dc9d89f60ae537efdbcfa3a086ad4fcb5e29
Tu Hoang <rebyn@me.com>
2013-06-08 14:29:18 +0200

    generate sid for new hydrant

--- app/models/hydrant.rb
+++ app/models/hydrant.rb
@@ -1,9 +1,16 @@
 class Hydrant < ActiveRecord::Base
   attr_accessible :latitude, :location, :longitude, :name, :sid, :user_id
 
+  before_validation :generate_sid
+
   geocoded_by :location
   after_validation :geocode
   reverse_geocoded_by :latitude, :longitude, :address => :location
-  
+
   belongs_to :user
+
+  protected
+  def generate_sid
+    self.sid = ('0'..'9').to_a.shuffle.first(10).join
+  end
 end
